7dbfc3a9f20582d41c1e5ef849d5d75383afc709
MiniMinja <minyoungheo.heo@gmail.com>
2022-06-07 12:53:12 -0700

    finished 2020 jan bronze usaco

--- general/src/U2020Janword.java
+++ general/src/U2020Janword.java
@@ -1,4 +1,53 @@
+import java.util.*;
+import java.io.*;
 
 public class U2020Janword {
-
+	
+	static BufferedReader in;
+	static PrintWriter out;
+	
+	static int N, K;
+	static String[] essay;
+	
+	static StringBuilder output;
+	
+	public static void main(String[] args) throws IOException{
+		init();
+		solve();
+		output();
+	}
+	
+	public static void init() throws IOException{
+		in = new BufferedReader(new FileReader("word.in"));
+		out = new PrintWriter(new BufferedWriter(new FileWriter("word.out")));
+		
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		essay = in.readLine().split(" ");
+		
+		output = new StringBuilder();
+		
+		in.close();
+	}
+	
+	public static void solve() {
+		int currentLineSize = 0;
+		for(int i = 0;i<essay.length;i++) {
+			if(essay[i].length() + currentLineSize > K) {
+				output.append("\n");
+				currentLineSize = 0;
+			}
+			
+			if(currentLineSize != 0) output.append(" ");
+			output.append(essay[i]);
+			currentLineSize += essay[i].length();
+		}
+	}
+	
+	public static void output() {
+		out.println(output);
+		out.close();
+	}
 }
